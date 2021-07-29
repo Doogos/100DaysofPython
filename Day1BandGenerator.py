10a7ca0bed63ebf0afdd31e6b0fdb98a28f8ae65
Doogos <88044014+Doogos@users.noreply.github.com>
2021-07-28 21:15:37 -0400

    Day 1
    Day 1 of 100

---
+++ Day1BandGenerator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("You've never seen a band generator like this before")
+#2. Ask the user for the city that they grew up in.
+city = input("Where are you from, rocker?")
+#3. Ask the user for the name of a pet.
+pet = input("What's the name of your most radical pet?")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your most awesome band name is " + city + " " + pet)
+#5. Make sure to input cursor shows on a new line, see example at:
+#   https://band-name-generator-end.appbrewery.repl.run/

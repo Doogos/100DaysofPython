5a8edce394d35412d153338ca23788714f416d29
Michael <88044014+Doogos@users.noreply.github.com>
2021-07-29 22:21:14 -0400

    Day 2Exercise

---
+++ 2.1Exercise.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+one = int(two_digit_number[0])
+two = int(two_digit_number[1])
+two_digit_number = one + two
+print(two_digit_number)
